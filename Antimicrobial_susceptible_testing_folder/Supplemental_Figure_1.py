ee9d295335cbdfb78896680f5903de36d67779c2
RachelAHickman <45556278+RachelAHickman@users.noreply.github.com>
2022-08-04 10:59:42 +0200

    Update Supplemental_Figure_1.py

--- Antimicrobial_susceptible_testing_folder/Supplemental_Figure_1.py
+++ Antimicrobial_susceptible_testing_folder/Supplemental_Figure_1.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 29 14:19:15 2020
+#"""
+#Created on Fri May 29 14:19:15 2020
 
-@author: rachi870
+#@author: rachi870
 """
 import pandas as pd
 import seaborn as sns
 
 #Import Data as a dataframe
-JPIA_MIC_data = pd.read_excel (r'./Antimicrobial_susceptible_testing_folder/antimicrobial_suceptible_raw_data.xlsx')
+JPIA_MIC_data = pd.read_excel (r '~/Antimicrobial_susceptible_testing_folder/antimicrobial_suceptible_raw_data.xlsx')
 JPIA_MIC_data.head()
 ###E.coli info first
 #Seperate dataframes for E. coli 
@@ -118,8 +118,8 @@ E_coli_TMP['Resistance_status'] = E_coli_TMP['MIC_Value(mg/L)'].apply(parse_valu
 #Remodel
 E_coli_Remodelled_JPIA_MIC_data = pd.concat([E_coli_AMP, E_coli_CHL, E_coli_CIP, E_coli_COL, E_coli_FOT, E_coli_GEN, E_coli_MERO, E_coli_TAZ, E_coli_TGC, E_coli_TMP])
 E_coli_Remodelled_JPIA_MIC_data2 =  pd.pivot_table (E_coli_Remodelled_JPIA_MIC_data, index=['bact_code'], columns=['Antibiotic_Drug'], values=['Resistance_status'])
-E_coli_Remodelled_JPIA_MIC_data.to_excel("/Users/rachi870/Desktop/JPIA_reduce_amu/EC_AST_profile1.xls")
-E_coli_Remodelled_JPIA_MIC_data2.to_excel("/Users/rachi870/Desktop/JPIA_reduce_amu/EC_AST_profile2.xls")
+E_coli_Remodelled_JPIA_MIC_data.to_excel("~/JPIA_reduce_amu/EC_AST_profile1.xls")
+E_coli_Remodelled_JPIA_MIC_data2.to_excel("~/JPIA_reduce_amu/EC_AST_profile2.xls")
 #Overview heatmaps 
 heatmap_E_coli = pd.pivot_table (E_coli_Remodelled_JPIA_MIC_data, index=['bact_code'], columns=['Antibiotic_Drug'], values=['Resistance_status'])
 cmap = sns.color_palette("coolwarm", 11)
@@ -141,21 +141,21 @@ ax = supplemental_figure_1_A.ax_heatmap
 ax.set_xticklabels(['COL', 'MERO', 'TGC', 'TMP', 'CHL', 'GEN', 'CIP', 'TAZ', 'AMP', 'FOT'], rotation=0)
 ax.set_ylabel('Bacterial Isolate (labelled by Farm-Source-Isolation media-Colony)')
 ax.set_xlabel('Antibiotic Drug')
-supplemental_figure_1_A.savefig('./Antimicrobial_susceptible_testing_folder/supplemental_figure_1_A.png', dpi=300)
+supplemental_figure_1_A.savefig('~/Antimicrobial_susceptible_testing_folder/supplemental_figure_1_A.png', dpi=300)
 #
 supplemental_figure_1_B = sns.clustermap(heatmap_E_coli_Contact_person, cmap = cmap)
 ax = supplemental_figure_1_B.ax_heatmap
 ax.set_xticklabels(['COL', 'MERO', 'TGC', 'TMP', 'AMP', 'FOT', 'CIP', 'TAZ', 'CHL', 'GEN'], rotation=0)
 ax.set_ylabel('Bacterial Isolate (labelled by Farm-Source-Isolation media-Colony)')
 ax.set_xlabel('Antibiotic Drug')
-supplemental_figure_1_B.savefig('./Antimicrobial_susceptible_testing_folder/supplemental_figure_1_B.png', dpi=300)
+supplemental_figure_1_B.savefig('~/Antimicrobial_susceptible_testing_folder/supplemental_figure_1_B.png', dpi=300)
 #
 supplemental_figure_1_C = sns.clustermap(heatmap_E_coli_Pig, cmap = cmap)
 ax = supplemental_figure_1_C.ax_heatmap
 ax.set_xticklabels(['AMP', 'FOT', 'COL', 'CIP', 'CHL', 'TMP', 'MERO', 'TGC', 'GEN', 'TAZ'], rotation=0)
 ax.set_ylabel('Bacterial Isolate (labelled by Farm-Source-Isolation media-Colony)')
 ax.set_xlabel('Antibiotic Drug')
-supplemental_figure_1_C.savefig('./Antimicrobial_susceptible_testing_folder/supplemental_figure_1_C.png', dpi=300)
+supplemental_figure_1_C.savefig('~/ Antimicrobial_susceptible_testing_folder/supplemental_figure_1_C.png', dpi=300)
 
 
 
